6f06ec6b79b8d854d6875867a10a21c311b9d3a2
kiwi-fruitiwi <zane.tian@gmail.com>
2021-08-30 08:02:33 -0700

    Add basic alignment

--- boid.py
+++ boid.py
@@ -3,12 +3,79 @@
 class Boid:
     def __init__(self):
         self.position = PVector(random(width), random(height))
-        self.velocity = PVector()
+        self.velocity = PVector().random2D().setMag(random(0.5, 1.5))
         self.acceleration = PVector()
+        self.max_force = 0.2
+        self.max_speed = 2
 
+
+    # update the boid's position, velocity, and acceleration
+    def update(self):
+        self.position.add(self.velocity)
+        self.velocity.add(self.acceleration)
+        self.acceleration = PVector(0, 0)
+                        
 
     def show(self):
-        strokeWeight(16)
+        strokeWeight(8)
         stroke(0, 0, 100)
         fill(0, 0, 100, 50)
         point(self.position.x, self.position.y)
+        
+    
+    # steer with limited perception of nearby neighbors
+    
+    # steer to avoid crowding local flockmates
+    def separation(self):
+        pass
+        
+    
+    # wrap off the edges
+    def edges(self):
+        if self.position.x > width:
+            self.position.x = 0
+        elif self.position.x < 0:
+            self.position.x = width
+            
+        if self.position.y > height:
+            self.position.y = 0
+        elif self.position.y < 0:
+            self.position.y = height
+    
+    
+    
+    # align this boid with all the other boids within a perception radius
+    def align(self, boids):
+        perception_radius = 40
+        total = 0
+        average = PVector(0, 0) # this is our desired velocity
+        
+        # find the average of the velocities of all the boids
+        for boid in boids:
+            distance = PVector.dist(self.position, boid.position)
+            # only calculate within a desired perception radius
+            if boid != self and distance < perception_radius:
+                total += 1 # count how many are within our radius to divide later for average
+                average.add(boid.velocity)                
+        
+        steering_force = average
+        
+        if total > 0:
+            steering_force.div(total) # this is our desired velocity!
+            # a steering force = desired velocity - actual velocity, 
+            # kind of like correcting error
+            
+            steering_force.setMag(self.max_speed)
+            steering_force = PVector.sub(steering_force, self.velocity)
+            steering_force.limit(self.max_force)
+        # else:            
+        #     # note that if we didn't find anything, we return the zero vector
+        #     return PVector(0, 0)
+     
+ 
+        return steering_force
+        
+        
+    def flock(self, boids):
+        alignment = self.align(boids)
+        self.acceleration.add(alignment) 
--- flocking.pyde
+++ flocking.pyde
@@ -1,8 +1,12 @@
 # Kiwi, 2021.08.39
 # Flocking Simulation with Daniel Shiffman at the Coding Train!
 # This is coding challenge #124
+# Flocking behavior follows:
+#    separation: steer to avoid crowding local flockmates
+#    alignment: steer toward the average heading of local flockmates
+#    cohesion: steer to move toward the average position of local flockmates
+#    object avoidance: ?
 #
-# 
 
 from boid import Boid
         
@@ -10,14 +14,14 @@ def setup():
     global flock
     
     size(640, 360)
-    frameRate(144)
+    # frameRate(144)
     colorMode(HSB, 360, 100, 100, 100)
     mono = createFont("terminus.ttf", 16);
     textFont(mono);   
     noSmooth()
     
     flock = []
-    for i in range(100):
+    for i in range(80):
         flock.append(Boid())
         
     
@@ -29,7 +33,10 @@ def draw():
     
     # display the entire flock
     for boid in flock:
+        boid.edges()
+        boid.flock(flock)
+        boid.update()
         boid.show()
     
-    
+    fill(0, 0, 100)
     text("{:.0f}".format(frameRate), 20, 30)
